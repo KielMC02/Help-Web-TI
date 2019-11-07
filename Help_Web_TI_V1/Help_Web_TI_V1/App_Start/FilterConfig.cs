e8a1259ae5dde7669426c49f2704bdeef9d4e889
KielMC02 <49813048+KielMC02@users.noreply.github.com>
2019-11-07 15:05:57 -0400

    Creacion de la Primera Version HELP WEB TI
    ***Creacion de la Primera Version.***
    -Nada de Codigo Insertado, solo el que genera el FrameWork ASP.
    -No tiene Capa de Login.

---
+++ Help_Web_TI_V1/Help_Web_TI_V1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Help_Web_TI_V1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Help_Web_TI_V1/Help_Web_TI_V1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Help_Web_TI_V1.MvcApplication" Language="C#" %>
